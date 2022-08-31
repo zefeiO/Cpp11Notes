d26da62c01702c63f8b07bdd5e511a5dc91860a5
JamesOu <jamesou@v1040-wn-rt-a-55-66.campus-dynamic.uwaterloo.ca>
2022-08-31 12:41:14 -0400

    added specialization in variadic_templates

--- variadic_templates/variadic_templates.cpp
+++ variadic_templates/variadic_templates.cpp
@@ -9,13 +9,22 @@ std::string to_string_impl(const T& t) {
     return ss.str();
 }
 
+/* specialization for uint8_t*/
+template <>
+std::string to_string_impl<uint8_t>(const uint8_t& t) {
+    std::stringstream ss;
+    ss << static_cast<int>(t);
+    return ss.str();
+}
+
+
 template <typename... Params>
 std::vector<std::string> to_string(const Params&... params) {
     return {to_string_impl(params)...};
 }
 
 int main() {
-    const auto vec = to_string("abc", 1, 1.3f, 5.3, 'r');
+    const auto vec = to_string("abc", 1, 1.3f, 5.3, 'r', (uint8_t)5);
     for (const auto& v:vec) {
         std::cout << v << std::endl;
     }
